da015561c2fa7b37e7903fed0d32c5a5cf2ec189
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-05 20:43:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', KeyDown)}
+function KeyDown(e){
+    var keyName = e.key;
+  if (keyName === codes[index]) {
+    index++;
+    if (index === codes.length) {
+     alert("Hurray!");
+     index = 0;
+  };
+  } else {
+index = 0;
 }
+}
+init();
